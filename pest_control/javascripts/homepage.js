ce4d7f4d448c107646323c4eec90375b6b097ee4
vishwajt <vishwajitdalve@gmail.com>
2024-10-02 16:34:45 +0530

    Final Commit

--- pest_control/javascripts/homepage.js
+++ pest_control/javascripts/homepage.js
@@ -2,3 +2,18 @@ function toggleMenu() {
     var menu = document.querySelector('.menu ul');
     menu.classList.toggle('active');
 }
+
+function updateRibbonLink() {
+    const ribbonLink = document.getElementById('ribbonLink');
+    
+    const screenWidth = window.innerWidth;
+    
+    if (screenWidth <= 480 || screenWidth > 480 && screenWidth <= 768) {
+        ribbonLink.href = 'tel:+919836383631';      // Considering this number as primary contact number
+    } else {
+        ribbonLink.href = '#';
+    }
+}
+
+window.onload = updateRibbonLink;
+window.onresize = updateRibbonLink;
